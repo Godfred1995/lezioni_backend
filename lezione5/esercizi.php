c8f4eca300140b23699dda124be12570df1107e1
Godfred1995 <119646805+Godfred1995@users.noreply.github.com>
2022-12-13 20:36:25 +0100

    refactoring

--- lezione5/esercizi.php
+++ lezione5/esercizi.php
@@ -1,33 +1,27 @@
 <?php
 
 /**
- *  1) puo' votare? l'età di una persona vedere se puo' votare if
- *  2) Dato un $myNumber 5, 1, 0 ,dire se e positivo, se è negetivo o se è zero
+ * 1) Può votare? Data l'età di una persona vedere se può votare if
+ * 2) Dato un $myNumber 5, -1, 0 , dire se è positivo, se è negativo o se è zero
  */
 
-
- // chiara
- 
 //primo esercizio
 
-/**  Variabili */
-$eta = 21;
-$legalAge = 18;
+$age = 17;
 
-/** check */
-
-if ($eta >= $legalAge) {
-    echo "Puoi votare!<br>";
+if ($age >= 18 && $age >= 25) {
+    echo " Puoi votare al senato!";
+} elseif ($age >= 18 && $age < 25) {
+    echo "Puoi votare solo alla camera ";
 } else {
-    echo "Non puoi votare! <br>";
+    echo "Non puoi votare.";
 }
 
 //Secondo esercizio
 
-$myNumber = 1;
-
-
-if (is_int($myNumber)) {
+/** Check number type */
+function numberType($myNumber)
+{
     if ($myNumber > 0) {
         echo "Il numero è positivo!";
     } elseif ($myNumber < 0) {
@@ -35,6 +29,25 @@ if (is_int($myNumber)) {
     } else {
         echo "Il numero è zero!";
     }
-} else {
-    echo "Non è un numero";
+}
+
+/** Check if is a real number or not */
+function isRealNumber($myNumber)
+{
+    if (is_int($myNumber)) {
+        $risultato = ($myNumber > 0) ? "positivo" : "è 0 oppure negativo";
+        echo 'Il numero è ' . $risultato;
+        numberType($myNumber);
+    } else {
+        echo "Non è un numero";
+    }
+}
+
+/** Main program */
+$myNumber = -1;
+$myNumber = 1 + $myNumber;
+
+if (!is_null($myNumber)) {
+    echo "Il numero vale " . $myNumber . '<br>';
+    isRealNumber($myNumber);
 }
